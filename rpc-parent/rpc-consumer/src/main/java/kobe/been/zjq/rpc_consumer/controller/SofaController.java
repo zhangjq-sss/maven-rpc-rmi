c6714c0ed28ecabc6acbaa76a13208e01bd71562
zhang.junquan <zhang.junquan@chinaebi.com>
2019-03-22 11:09:33 +0800

    SOFARPC增加H2C协议以及删除无用的代码

--- rpc-parent/rpc-consumer/src/main/java/kobe/been/zjq/rpc_consumer/controller/SofaController.java
+++ rpc-parent/rpc-consumer/src/main/java/kobe/been/zjq/rpc_consumer/controller/SofaController.java
@@ -5,6 +5,10 @@ import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
+
+import kobe.been.zjq.rpc_service.H2CService;
 import kobe.been.zjq.rpc_service.IRemoteService;
 import kobe.been.zjq.rpc_service.IRemoteServiceRest;
 
@@ -17,6 +21,9 @@ public class SofaController {
 	IRemoteServiceRest restSyncService;
 	@Resource(name="dubboSyncServiceReference")
 	IRemoteService dubboSyncService;
+	
+	@SofaReference(binding = @SofaReferenceBinding(bindingType = "h2c"), jvmFirst = false)
+	H2CService h2cSyncService;
 	
 	@RequestMapping("/bolt")
 	public String getByBindingBolt() {
@@ -34,5 +41,11 @@ public class SofaController {
 	public String getByBindingDubbo() {
 		
 		return dubboSyncService.getUserById(3l).toString();
+	}
+	
+	@RequestMapping("/h2c")
+	public String getByBindingH2c() {
+		
+		return h2cSyncService.getUserById(4l).toString();
 	}
 }
